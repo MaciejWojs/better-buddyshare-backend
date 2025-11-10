0d5edb2e8dfcc4966182dc2f269dd6ba336646d7
MaciejWojs <misiek111403@gmail.com>
2025-11-10 17:19:05 +0100

    feat: add Subscription, TopStreamer, UserAsSubscriber, and UserAsSubscriberWithStreamer types

---
+++ src/types/db/Subscription.ts
@@ -0,0 +1,6 @@
+export type Subscription = {
+  subscription_id: number;
+  user_id: number;
+  streamer_id: number;
+  subscribed_since: Date;
+};
---
+++ src/types/db/UserAsSubscriberWithStreamer.ts
@@ -0,0 +1,7 @@
+export type UserAsSubscriberWithStreamer = {
+  user_id: number;
+  streamer_id: number;
+  subscribed_since: Date;
+  streamer_username: string;
+  user_username: string;
+};
