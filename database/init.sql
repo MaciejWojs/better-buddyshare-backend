689a92dc616fb225f1979edf34c7c4dddd3eb046
MaciejWojs <misiek111403@gmail.com>
2025-11-15 12:46:54 +0100

    refactor: update Docker Compose and initialization script for PostgreSQL with JWT configuration and cron jobs

--- database/init.sql
+++ database/init.sql
@@ -1,2 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS "pg_cron";
 
+SELECT
+  cron.schedule (
+    '0 * * * *', -- every hour
+    'SELECT cleanup_expired_sessions_tokens();'
+  );
+
+SELECT
+  cron.schedule (
+    'daily_cleanup_revoked_tokens', -- task name
+    '0 3 * * *', -- schedule: every day at 3:00
+    'SELECT cleanup_revoked_tokens_older_than(30);' -- remove tokens revoked more than 30 days ago
+  );
